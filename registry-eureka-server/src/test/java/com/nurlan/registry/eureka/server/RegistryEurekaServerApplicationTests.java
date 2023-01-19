d0f446808be0fdbe41666c1bc4478565d473c194
nurlanlm <53323175+nurlanlm@users.noreply.github.com>
2023-01-19 12:03:44 +0400

    Add files via upload

---
+++ registry-eureka-server/src/test/java/com/nurlan/registry/eureka/server/RegistryEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurlan.registry.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
